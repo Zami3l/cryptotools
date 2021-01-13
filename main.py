2edd8a5f9727f9500670391b9d099274857fc9df
zami3l <null@zami3l.com>
2021-01-13 23:22:14 +0100

    refactor(main): ajout encryption shift xor et hash md5

--- main.py
+++ main.py
@@ -3,23 +3,29 @@
 
 import argparse, logging, sys
 import clipboard
-import hashing, encoding
+import hashing, encoding, encryption
 
 def check_args(_agrs=None):
     
     # Init Argparse
-    parser = argparse.ArgumentParser(description='Hashtools')
+    parser = argparse.ArgumentParser(description='Cryptotools')
     
+    parser.add_argument('-sha1', action="store_true", help="Use sha1 hash algorithmn")
+    parser.add_argument('-sha256', action="store_true", help="Use sha256 hash algorithmn")
+    parser.add_argument('-sha512', action="store_true", help="Use sha512 hash algorithmn")
+    parser.add_argument('-md5', action="store_true", help="Use md5 hash algorithmn")
+    parser.add_argument('-b16', action="store_true", help="Encode with base 16")
+    parser.add_argument('-b32', action="store_true", help="Encode with base 32")
+    parser.add_argument('-b64', action="store_true", help="Encode with base 64")
+    parser.add_argument('-rot13', action="store_true", help="Encryption Caesar")
+    parser.add_argument('-xor', action="store_true", help="Encryption xor")
+    parser.add_argument('-c', '--clip', action="store_true", help="Copy to clipboard")
+    parser.add_argument('-k', '--key', type=str, metavar='key', help="Key for encryption")
+    parser.add_argument('-up', '--upper', action="store_true", help="Result with uppercase")
+    parser.add_argument('-hid', '--hidden', action="store_true", help="Hidden result")
     parser.add_argument('-v', '--verbose', action='store_true', help="Mode verbose")
     parser.add_argument('-d', '--debug', action='store_true', help="Mode debug")
-    parser.add_argument('-sha1', '--sha1', action="store_true", help="Use sha1 hash algorithmn")
-    parser.add_argument('-sha256', '--sha256', action="store_true", help="Use sha256 hash algorithmn")
-    parser.add_argument('-sha512', '--sha512', action="store_true", help="Use sha512 hash algorithmn")
-    parser.add_argument('-b16', '--b16', action="store_true", help="Encode with base 16")
-    parser.add_argument('-b32', '--b32', action="store_true", help="Encode with base 32")
-    parser.add_argument('-b64', '--b64', action="store_true", help="Encode with base 64")
     parser.add_argument('text', type=str, metavar='text', help="Text for Encode/Hash")
-    parser.add_argument('-c', '--clip', action="store_true", help="Copy to clipboard")
 
     args = parser.parse_args()
     
@@ -36,6 +42,9 @@ def action(mode):
     if mode.sha512:
         result = hashing.sha512(mode.text)
     
+    if mode.md5:
+        result = hashing.md5(mode.text)
+
     if mode.b16:
         result = encoding.b16(mode.text)
     
@@ -45,7 +54,23 @@ def action(mode):
     if mode.b64:
         result = encoding.b64(mode.text)
 
-    return result
+    if mode.rot13:
+        result = encryption.Shift_Cipher().ascii_letter(mode.text, 13, 1)
+    
+    if mode.xor:
+        result = encryption.Xor_Cipher().str(mode.text, mode.key)
+
+    # Uppercase
+    if mode.upper:
+        result = result.upper()
+
+    # View result
+    if not mode.hidden:
+        print(result)
+
+    # Copy result
+    if mode.clip:
+        clipboard.copy(result)
 
 def main():
 
@@ -55,15 +80,9 @@ def main():
 
     args = check_args(sys.argv[1:])
 
-    # Return
-    result = action(args)
+    # Run
+    action(args)
 
-    # View result
-    print(result)
-
-    # Copy result
-    if args.clip:
-        clipboard.copy(result)
 
 if __name__ == "__main__":
 
