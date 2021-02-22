ee3737aee91ae3522bf1b49bb4d336514274bc0c
zami3l <null@zami3l.com>
2021-02-22 19:16:30 +0100

    refactor(tools): ajout logs debug

--- functions/tools.py
+++ functions/tools.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 # coding : utf-8
 
+import logging
 
 def read(file):
 
     with open(file, 'rb') as file:
-        return file.read()
 
+        fileRead = file.read()
+        logging.debug('\n===READ FILE===\n%s\n===============', fileRead)
+
+        return fileRead
 
 def write(data, file):
 
     with open(file, 'wb') as file:
+
+        logging.debug('\n===WRITE FILE===\n%s\n================', data)
         file.write(data)
