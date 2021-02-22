d5dd507089fe9caa139ac3ed3946d6a14b4fcbd6
zami3l <null@zami3l.com>
2021-02-22 19:18:39 +0100

    feat(tools): ajout fonction case_letter et letter_position La fonction case_letter permet de définir si le caractère ascii est de type alphabet et si celle ci est en majuscule ou minuscule. Dans le cas ou ce n'est pas un caractère de l'alphabet, alors il renvoie 0.
    La fonction letter_position renvoie la position de la lettre dans
    l'alphabet.

--- functions/tools.py
+++ functions/tools.py
@@ -18,3 +18,36 @@ def write(data, file):
 
         logging.debug('\n===WRITE FILE===\n%s\n================', data)
         file.write(data)
+
+def letter_position(letter):
+    
+    # Lowercase
+    if letter >= 97 and letter <= 122:
+    
+        return letter - 97
+    
+    # Uppercase
+    elif letter >= 65 and letter <= 90:
+
+        return letter - 65
+    
+    else:
+
+        return 0
+
+def case_letter(letter):
+    
+    # Lowercase
+    if letter >= 97 and letter <= 122:
+    
+        return 'lowercase'
+    
+    # Uppercase
+    elif letter >= 65 and letter <= 90:
+
+        return 'uppercase'
+
+    # Other
+    else:
+
+        return 'other'
