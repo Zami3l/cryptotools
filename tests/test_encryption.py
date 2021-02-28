ffa84a9075cb3acbf5f0d92fb08d2760317a3c5a
zami3l <null@zami3l.com>
2021-02-28 18:34:37 +0100

    feat(tests): ajout test pour substitution cipher ascii extended

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -13,6 +13,9 @@ class Test_Encryption(unittest.TestCase):
     plainText = "Hi there, I'm Zami3l".encode('utf8')
     key = "Zami3l".encode('utf-8')
 
+    plainText2 = "0123456789".encode('utf8')
+    key2 = "#$%&'()*".encode('utf-8')
+
     def test_shift_cipher(self):
         
         cipherText = Shift_Cipher().ascii_letter('E', self.plainText, 8, 3)
@@ -31,3 +34,8 @@ class Test_Encryption(unittest.TestCase):
         self.assertEqual(cipherText.decode('utf8'), "Gi fpecd, I'y Haxh3l")
         plainText = Substitution_Cipher().ascii_letter('D', cipherText, self.key)
         self.assertEqual(plainText, self.plainText)
+
+        cipherText = Substitution_Cipher().ascii_extented('E', self.plainText2, self.key2)
+        self.assertEqual(cipherText.decode('utf8', 'ignore'), "SUWY[]_a[]")
+        plainText = Substitution_Cipher().ascii_extented('D', cipherText, self.key2)
+        self.assertEqual(plainText, self.plainText2)
